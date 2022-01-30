63a93190c7e7f29f8d503568cba55893bf3857ac
Lucas Toledo <lucasmoraisss@gmail.com>
2022-01-30 20:32:26 -0300

    strings pratica

--- Backend/21-funcoes-sql-joins-e-normalizacao/21_1-funcoes-mais-usadas-em-sql/aulas/strings.sql
+++ Backend/21-funcoes-sql-joins-e-normalizacao/21_1-funcoes-mais-usadas-em-sql/aulas/strings.sql
@@ -18,8 +18,17 @@ SELECT SUBSTRING(title, 5, 2) FROM sakila.film WHERE film_id = 1;
 SELECT SUBSTRING(title, 5) FROM sakila.film WHERE film_id = 1;
 ##################################################################
 
+#1
 SELECT UCASE('trybe');
 
+#2
+SELECT REPLACE('Você já ouviu falar do DuckDuckGo?', 'DuckDuckGo', 'Google');
 
+#3
+SELECT CHAR_LENGTH('Uma frase qualquer');
 
+#4
+SELECT SUBSTRING('A linguagem Javascript está entre as mais usadas', 13, 10);
 
+#5
+SELECT LCASE('RUA NORTE 1500, SÃO PAULO, BRASIL')
