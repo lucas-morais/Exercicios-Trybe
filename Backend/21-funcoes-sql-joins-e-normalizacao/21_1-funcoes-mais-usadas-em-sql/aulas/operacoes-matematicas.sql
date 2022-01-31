a4673148915f80f6fc1b3dc09eeef6621d2b2ef7
Lucas Toledo <lucasmoraisss@gmail.com>
2022-01-30 21:11:07 -0300

    operacoes-matematicas

---
+++ Backend/21-funcoes-sql-joins-e-normalizacao/21_1-funcoes-mais-usadas-em-sql/aulas/operacoes-matematicas.sql
@@ -0,0 +1,19 @@
+#1
+SELECT IF(15 MOD 2, 'Ímpar', 'Par');
+
+#2
+SELECT 220 DIV 12;
+
+#3
+SELECT 220 - (18 * 12);  
+
+######################################
+
+#1
+SELECT ROUND(15 + (RAND()*5));
+
+#2
+SELECT ROUND(15.7515971, 5);
+
+#3
+SELECT FLOOR(39.494);
