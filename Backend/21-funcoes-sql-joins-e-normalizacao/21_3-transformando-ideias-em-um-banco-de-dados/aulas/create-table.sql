81efb4708810e499e8b2c57f9336212a8990998a
Lucas Toledo <lucasmoraisss@gmail.com>
2022-02-02 15:37:33 -0300

    creating database

---
+++ Backend/21-funcoes-sql-joins-e-normalizacao/21_3-transformando-ideias-em-um-banco-de-dados/aulas/create-table.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE IF NOT EXISTS albuns;
+
+USE albuns;
+
+CREATE TABLE artista (
+    artista_id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50)
+)  ENGINE=INNODB;
+
+CREATE TABLE estiloMusical (
+    estilo_id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50)
+)  ENGINE=INNODB;
+
+
+CREATE TABLE album(
+	album_id INT PRIMARY KEY AUTO_INCREMENT,
+    artist_id INT NOT NULL,
+    titulo VARCHAR(100) NOT NULL,
+    preco DECIMAL(5,2) NOT NULL,
+    estilo_id INT NOT NULL,
+    FOREIGN KEY (artist_id) REFERENCES artista(artista_id),
+    FOREIGN KEY (estilo_id) REFERENCES estiloMusical(estilo_id)
+) ENGINE=InnoDB;
