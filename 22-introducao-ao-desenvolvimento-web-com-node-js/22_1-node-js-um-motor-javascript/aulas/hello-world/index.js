b8eec56c92d46ea41c0bdfdbacb7f3b035a732cc
Lucas Toledo <lucasmoraisss@gmail.com>
2022-02-05 10:08:19 -0300

    deletados

--- 22-introducao-ao-desenvolvimento-web-com-node-js/22_1-node-js-um-motor-javascript/aulas/hello-world/index.js
+++
@@ -1,8 +0,0 @@
-const readline = require('readline-sync');
-
-// console.log('Hello, World!');
-
-const name = readline.question('Qual seu nome? ');
-const age = readline.question('Qual sua idade? ');
-
-console.log(`Hello, ${name}! You are ${age} yars old!`)
