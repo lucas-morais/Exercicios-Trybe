9004b631f0a764512131a3d52432c68e80269f6b
Lucas Toledo <lucasmoraisss@gmail.com>
2022-03-08 16:12:05 -0300

    aulas n:n e transactions

--- Backend/24-orm-e-autenticacao/24_2-orm-associations/aulas/index.js
+++ Backend/24-orm-e-autenticacao/24_2-orm-associations/aulas/index.js
@@ -1,5 +1,11 @@
 const express = require('express');
-const { Address, Employee } = require('./models');
+const { Address, Employee, Book, User } = require('./models');
+
+const Sequelize = require('sequelize');
+
+const config = require('./config/config');
+
+const sequelize = new Sequelize(config.development);
 
 const PORT = 3000;
 
@@ -17,6 +23,78 @@ app.get('/employees', async(_req, res) => {
     console.log(e);
     res.status(500).json({ message: 'Ocorreu um erro' });
   }
+})
+
+app.get('/employees/:id', async(req, res) => {
+  try {
+    const { id } = req.params;
+    const employee = await Employee.findOne({
+      where:{ id },
+      include:[{ 
+        model: Address, 
+        as: 'addresses',
+        attributes: { exclude: ['number']}
+      }]
+    })
+
+    if(!employee) {
+      return res.status(404).json({ message: 'Funcionário não encontrado'})
+    }
+
+    return res.status(200).json(employee);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: 'Algo deu errado'});
+  }
+});
+
+app.get('/userbooks/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findOne({
+      where: {
+        userId: id
+      },
+      include: [{ model: Book, as: 'books', trough: { attributes: [] } }]
+    });
+
+    if(!user) {
+      return res.status(404).json({ message: 'Ususário não encontrado' })
+    }
+
+    return res.status(200).json(user);
+  } catch( error) {
+    console.log(error.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  }
+})
+
+app.post('/employees', async (req, res) => {
+  const t = await sequelize.transaction();
+
+  try {
+    const { firstName, lastName, age, city, street, number } = req.body;
+    
+    await sequelize.transaction( async (t) => {
+      const employee = await Employee.create(
+        {firstName, lastName, age},
+        {transaction: t},
+      )
+
+      console.log(employee)
+      await Address.create(
+        {city, street, number, employeeId: employee.id },
+        { transaction: t}
+      )
+
+      return res.status(201).json({message: 'Cadastrado com sucesso'});
+    })
+  
+
+  } catch(error) {
+    console.log(error.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  } 
 })
 
 
