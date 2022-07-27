319a4353ed2b478c4b1609dd01a42d33605cf8ae
Lucas Toledo <lucasmoraisss@gmail.com>
2022-07-27 09:12:19 -0300

    insertion sort, selection sort

---
+++ CS/36_3-algoritmos/aulas/selection_sort.py
@@ -0,0 +1,29 @@
+def search(numbers, start, end):
+    min_element = numbers[start]
+    min_element_index = start
+
+    for i in range(start + 1, end):
+        if numbers[i] < min_element:
+            min_element = numbers[i]
+            min_element_index = i
+
+    return min_element_index
+
+
+def selection_sort(numbers):
+
+    n = len(numbers)
+
+    for index in range(n - 1):
+        min_element_index = search(numbers, index, n)
+        numbers[index], numbers[min_element_index] = (
+            numbers[min_element_index],
+            numbers[index],
+        )
+    return numbers
+
+
+numbers = [7, 5, 9, 2, 6, 8]
+print(f"Lista inicial: {numbers}")
+ordered_numbers = selection_sort(numbers)
+print(f"Lista final: {ordered_numbers}")
